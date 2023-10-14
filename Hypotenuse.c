f3fbc4d5c888dc7abd7b0e407a6a34be5e86b265
23p31a4274 <147792052+23p31a4274@users.noreply.github.com>
2023-10-14 10:55:54 +0530

    #include<stdio.h> #include<math.h> int main() {    int a,b;    float c;    scanf("%d%d",&a,&b);    c=sqrt(a*a+b*b);    printf("%.2f",c); }

--- Hypotenuse.c
+++ Hypotenuse.c
@@ -1,11 +1,10 @@
-c#include<stdio.h>
+#include<stdio.h>
+#include<math.h>
 int main()
 {
    int a,b;
    float c;
    scanf("%d%d",&a,&b);
-   c=a-b;
-   c=c/a;
-   c=c*100;
+   c=sqrt(a*a+b*b);
    printf("%.2f",c);
-}}
+}
