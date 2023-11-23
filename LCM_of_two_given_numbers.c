819da50aa1e9f764e675b0b4ca52379ee011d5ec
23p31a4274 <147792052+23p31a4274@users.noreply.github.com>
2023-11-23 09:38:59 +0530

    #include<stdio.h> int main() {     int i,a,b,n;     scanf("%d %d",&a,&b);     for(i=1;i<=b;i++)     {         n=i*a;         if(n%b==0)         {             printf("%d",n);             break;         }     } }

--- LCM_of_two_given_numbers.c
+++ LCM_of_two_given_numbers.c
@@ -1,13 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int i,n1,n2,gcd,lcm;
-    scanf("%d%d",&n1,&n2);
-        for(i=1;i<=n1 && i<=n2;i++)
+    int i,a,b,n;
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=b;i++)
+    {
+        n=i*a;
+        if(n%b==0)
         {
-            if(n1%i==0 && n2%i==0)
-            gcd=i;
+            printf("%d",n);
+            break;
         }
-        lcm=(n1*n2)/gcd;
-        printf("%d",lcm);
+    }
 }
